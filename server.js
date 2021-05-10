0222680ea4d732fe6001215544e54f70d7768907
Dagger Silver <shitcodder@gmail.com>
2021-05-10 11:03:46 +0530

    d

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const io = require("socket.io")(3000, {
+const express = require("express");
+const app = express();
+
+app.listen(3000);
+
+
+const io = require("socket.io")(app, {
     cors: {
         origin: "http://127.0.0.1:5500",
         methods: ["GET", "POST"]
